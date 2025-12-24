fa2ffc947e2e4f86b75072540f0dd6039a441fab
omid-aria <omidesmaeili479@gmail.com>
2025-12-24 12:33:56 -0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,50 +1,16 @@
-const CACHE_NAME = 'saffron-shop-v1';
-
-self.addEventListener('install', event => {
-  console.log('SW: Installing');
+// Service Worker خیلی ساده و امن
+self.addEventListener('install', () => {
+  console.log('SW: Installed');
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('SW: Activating');
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('SW: Deleting old cache', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', () => {
+  console.log('SW: Activated');
 });
 
-self.addEventListener('fetch', event => {
-  // فقط برای درخواست‌های GET پاسخ بده
-  if (event.request.method !== 'GET') {
-    return;
+// فقط صفحه اصلی رو سرو می‌کنه، بقیه رو به شبکه بفرست
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request));
   }
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // اگر در کش بود، برگردان
-        if (response) {
-          console.log('SW: Serving from cache', event.request.url);
-          return response;
-        }
-        
-        // در غیر این صورت، از شبکه بگیر
-        console.log('SW: Fetching from network', event.request.url);
-        return fetch(event.request)
-          .catch(() => {
-            // اگر شبکه هم کار نکرد، صفحه اصلی رو برگردان
-            if (event.request.destination === 'document') {
-              return caches.match('/index.html');
-            }
-          });
-      })
-  );
 });
