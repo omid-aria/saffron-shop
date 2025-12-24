424eaaebf43d0d1a08ccbd72131d27503b200054
omid-aria <omidesmaeili479@gmail.com>
2025-12-24 09:35:04 -0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,33 @@
+const CACHE_NAME = 'saffron-shop-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/data.json'
+];
 
+// نصب Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// فعال‌سازی و مدیریت درخواست‌ها
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // اگر در کش بود، برگردان
+        if (response) {
+          return response;
+        }
+        // در غیر این صورت، درخواست را از شبکه بگیر
+        return fetch(event.request);
+      }
+    )
+  );
+});
