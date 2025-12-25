20ed69f59d5c7fa2e8729898f34938893e62aeb8
omid-aria <omidesmaeili479@gmail.com>
2025-12-24 19:29:37 -0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// استفاده از './' باعث می‌شود فایل‌ها در هر مسیری (ریشه یا زیرپوشه) به درستی لود شوند
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
